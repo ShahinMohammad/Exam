d5537f1b6b9c974494e03e0410dfd0dff9d8a9b9
ShahinMohammad <shahinmohammad24rb@gmail.com>
2016-12-13 12:21:50 +0600

    git working here

---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Exam
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function exam12_16_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'exam12_16_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'exam12_16_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function exam12_16_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
